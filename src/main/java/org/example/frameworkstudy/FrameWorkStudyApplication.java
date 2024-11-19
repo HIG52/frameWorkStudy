65caa268679b6e08355e790b8ef45b7ff72b8fe2
홍준영 <hingjy1997@gmail.com>
2024-11-20 00:13:47 +0900

    최초 커밋

---
+++ src/main/java/org/example/frameworkstudy/FrameWorkStudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.frameworkstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameWorkStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameWorkStudyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/frameworkstudy/entity/BoardEntity.java
@@ -0,0 +1,27 @@
+package org.example.frameworkstudy.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class BoardEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BOARDID", nullable = false)
+    private Integer id;
+
+    @Column(name = "TITLE", nullable = false)
+    private String title;
+
+    @Lob
+    @Column(name = "CONTENTS", nullable = false)
+    private String contents;
+
+    @Column(name = "AUTHOR", nullable = false, length = 100)
+    private String author;
+
+}
