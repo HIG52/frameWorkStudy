734b00e5bde7aa9e1845451eb0bad6b84982b9ed
홍준영 <hongjy1997@naver.com>
2024-11-28 00:55:21 +0900

    테스트 코드 작성

--- src/main/java/org/example/frameworkstudy/dto/UserJoinDTO.java
+++ src/main/java/org/example/frameworkstudy/dto/UserJoinDTO.java
@@ -1,9 +1,8 @@
 package org.example.frameworkstudy.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.Objects;
 
 //실제 엔티티 객체를 통해 데이터를 전달하지 않고 DTO를 통해 데이터를 주고받기 위해 생성
 @Data
